9343bab0c9724ce0e2c7542ca01f5e5c53f9d95d
Harsh Chandrakar <61453497+harshedcast@users.noreply.github.com>
2024-10-18 15:19:28 +0530

    weather-app

---
+++ weather-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WeatherDetailsComponent } from './weather-details/weather-details.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WeatherDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ weather-app/src/app/weather-details/weather-details.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-weather-details',
+  templateUrl: './weather-details.component.html',
+  styleUrls: ['./weather-details.component.scss']
+})
+export class WeatherDetailsComponent {
+
+  constructor(private httpClient: HttpClient){
+
+  }
+
+  temp:any = 20;
+  humidity = 4;
+  city='';
+  baseWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+
+  urlSuffix = "&units=metric&APPID=aeb5f03a2edf1f30f04a0300da7e7e6c";
+
+  getWeather(){
+    this.httpClient.get(this.baseWeatherURL+this.city+this.urlSuffix).subscribe((response:any)=>{
+      console.log(response);
+      this.temp = response['main']['temp'];
+      this.humidity = response['main']['humidity'];
+    })
+  }
+
+}
